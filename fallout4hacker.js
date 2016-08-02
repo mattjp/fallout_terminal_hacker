469f4c587daceb689cb2d5d536764e8abb808ac2
Matt Peretick <mattjp@users.noreply.github.com>
2016-08-02 14:14:05 -0400

    ughhhh

--- fallout4hacker.js
+++ fallout4hacker.js
@@ -12,13 +12,15 @@ function generate_guess() {
 		$("#word8").val()
 	];
 
-	// Booleans
+	// Bools
 	var status = [1, 1, 1, 1, 1, 1, 1, 1];
 
 	// Variable inputs
 	var guess_num = $("#guess_num").text();
 	var correct_chars = $("#correct_chars").val();
-	// var answer = $("#guess_word").text();
+	var cur_guess = $("#guess_word").text();
+
+	// check that all words are the same length
 
 	if(guess_num == 0) {
 		$("#guess_word").text(words[0]);
@@ -26,15 +28,16 @@ function generate_guess() {
 	}
 	else if(guess_num == 1) {
 		if(correct_chars == 0) {
-			var cur_guess = $("#guess_word").text();
 			$.each(words, function(index, value) {
 				for(var i = 0; i < value.length; i++) {
 					if(cur_guess[i] == value[i]) {
 						// add line through this word
+						// break
 					}
 				}
 			});
 		}
+
 		//alert(sum);
 	}
 	
