d051c559eb7f61eadc187e95b7b05fb8e340132c
Matt Peretick <mattjp@users.noreply.github.com>
2016-08-02 12:32:12 -0400

    Add files via upload

--- fallout4hacker.js
+++ fallout4hacker.js
@@ -1,6 +1,29 @@
 function generate_guess() {
-	var word_one = $("#word1");
-	alert(word_one);
-	// var guess = $("#guess_word");
-	// guess = word_one;
+	var word = {}; 			// Word object
+	var word_array = []; 	// Array of Words
+
+	str = $("#word1").val();
+	status = 1;
+
+	// var words = [
+	// 	$("#word1").val(),
+	// 	$("#word2").val(),
+	// 	$("#word3").val(),
+	// 	$("#word4").val(),
+	// 	$("#word5").val(),
+	// 	$("#word6").val(),
+	// 	$("#word7").val(),
+	// 	$("#word8").val()
+	// ];
+
+	// Variable inputs
+	var guess_num = $("#guess_num").text();
+	var correct_chars = $("#correct_chars").val();
+	var answer = $("#guess_word").text();
+	
+
+
+
+
+	alert(words[0]);
 }
